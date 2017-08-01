49ef750f3426cdb0c2a7465f47d31d7702166415
frankie <frankie_lrh@163.com>
2017-08-02 00:21:52 +0800

    tesy

---
+++ f2f/app/src/main/java/com/i_edu/f2f/MainActivity.java
@@ -0,0 +1,14 @@
+package com.i_edu.f2f;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
